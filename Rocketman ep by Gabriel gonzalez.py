6e62c5a8d80de6ee9bdb332464505a5b3cac6ced
Sadboy420 <gabriel.gonzalezv@sansano.usm.cl>
2018-01-21 15:10:15 -0300

    Add files via upload

--- Rocketman ep by Gabriel gonzalez.py
+++ Rocketman ep by Gabriel gonzalez.py
@@ -1,6 +1,8 @@
 from random import choice
-def crear_personaje():
-    Personajes=open('personajes.txt','a')
+from Tkinter import *
+
+def crear_personaje(x):
+    Personajes=open(x+'_personajes.txt','a')
     nombre = raw_input("Ingrese nombre del personaje:")
     stats=dict()
     gen=dict()
@@ -26,25 +28,51 @@ def crear_personaje():
     Personajes.write(str(gen)+'\n')
     Personajes.close()
     return Personajes
+
+def crear_item():
+    return
+
+def inv_pers(uni):
+
+def historia(x):
+    while True:
+        h=open(x+'_historia.txt','a')
+        text=raw_input('Ingrese hechos para salir escriba guardar:')
+        if text=='guardar':
+            h.close()
+            break
+        else:
+            h.write(text+'\n')
+    return h
+
+#v0=Tk()
+#v0.config(bg='red')
+#v0.geometry('500x500')
+#b1=Button(v0,text="ir a los dados",command=dado())
+#v1=Toplevel(v0)
+#v0.mainloop()
+uni=raw_input('ingrese nombre del universo: ')
 while True:
-    print '|-------------------------------------------------|\n|Crear personaje: 1 \n|Ver personajes: 2\n|Deletea tus personajes: 3\n|Acabar con mi vida: 4\n|-------------------------------------------------|'
+    print '|-------------------------------------------------|\n|Crear personaje: 1 \n|Iniciar/Resumir historia:2 \n|Ver personajes: 3\n|Deletea tus personajes: 4\n|Acabar con mi vida: 5\n|-------------------------------------------------|'
     xd= int(raw_input(' Ingrese opcion:'))
     if xd== 1:
-        crear_personaje()
-    if xd==2:
-        xdd=open('personajes.txt','r')
+        crear_personaje(uni)
+    if xd==3:
+        xdd=open(uni+'_personajes.txt','r')
         for linea in xdd:
             print linea
-    if xd==3:
+    if xd==2:
+        #historia(uni)
+    if xd==4:
         print '¿Estas seguro?'
         nya=str(raw_input())
         if nya=='si':
-            deletiao=open('personajes.txt','w')
+             deletiao=open(uni+'.txt','w')
             deletiao.close()
         if nya=='no':
-            print ':('
-    if xd==4:
-        break
+            #print ':('
+    if xd==5:
+       break
 #Hecho por Gabriel Gonzalez
 #Contacto: xD
         
